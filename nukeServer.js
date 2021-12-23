d7b92533dd23c53ee2703beab6e5c534aa1e0a4b
SjurSutterudSagen <sjur@sutterudsagen.no>
2021-12-23 07:59:55 +0100

    Fix typos

--- nukeServer.js
+++ nukeServer.js
@@ -4,48 +4,48 @@
 
 export async function main(ns, serverToNuke) {
     const args = ns.flags([['help', false]]);
-    const serverToHack = args._[0];
+    const targetServer = args._[0];
     let hackToolsUsed = 0;
-    let rootAccess = ns.hasRootAccess(serverToNuke);
-    const requiredPorts = ns.getServerNumPortsRequired(serverToNuke);
+    let rootAccess = ns.hasRootAccess(targetServer);
+    const requiredPorts = ns.getServerNumPortsRequired(targetServer);
 
     if (!rootAccess && requiredPorts !== 0) {
-        ns.tprint('Trying to open ports on: ' + serverToNuke);
+        ns.tprint('Trying to open ports on: ' + targetServer);
 
         if (ns.fileExists('BruteSSH.exe', home)) {
-            ns.brutessh(serverToNuke);
+            ns.brutessh(targetServer);
             hackToolsUsed++;
         }
 
         if (ns.fileExists('FTPCrack.exe', home)) {
-            ns.ftpcrack(serverToNuke);
+            ns.ftpcrack(targetServer);
             hackToolsUsed++;
         }
 
         if (ns.fileExists('relaySMTP.exe', home)) {
-            ns.relaysmtp(serverToNuke);
+            ns.relaysmtp(targetServer);
             hackToolsUsed++;
         }
 
         if (ns.fileExists('HTTPWorm.exe', home)) {
-            ns.httpworm(serverToNuke);
+            ns.httpworm(targetServer);
             hackToolsUsed++;
         }
         
         if (ns.fileExists('SQLInject.exe', home)) {
-            ns.sqlinject(serverToNuke);
+            ns.sqlinject(targetServer);
             hackToolsUsed++;
         }
     }
 
     if (requiredPorts <= hackToolsUsed && !hasRootAccess) {
-        ns.tprint('Trying to nuke: ' + serverToNuke)
-        ns.nuke(serverToNuke);
-        rootAccess = ns.hasRootAccess(serverToNuke);
+        ns.tprint('Trying to nuke: ' + targetServer)
+        ns.nuke(targetServer);
+        rootAccess = ns.hasRootAccess(targetServer);
     }
 
     if (!rootAccess) {
-        ns.tprint('Could not gain root access to: ' + serverToNuke);
+        ns.tprint('Could not gain root access to: ' + targetServer);
     }
 
     return rootAccess;
